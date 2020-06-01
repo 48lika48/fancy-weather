93827b4a556cdaa4bc5e586d43594b3e06af5b2c
48lika48 <48lika48@mail.ru>
2020-06-01 23:31:33 +0300

    feat: add coordinates

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ let tokenForWeather = '07eb68c0596b4dd2801b11aab9c9e362';
 let currentCity = document.getElementById('location');
 let dataTime = document.getElementById('data-time');
 
+let latitude = document.getElementById('latitude');
+let longitude = document.getElementById('longitude');
+
 
 async function getGeolocation() {
     const url = `https://ipinfo.io/json?token=${tokenForGeolocation}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
-}
-
-async function getWeather(city) {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ua&units=metric&APPID=${tokenForWeather}`;
     const res = await fetch(url);
     const data = await res.json();
     return data;
@@ -23,6 +19,28 @@ getGeolocation().then((data) => {
     currentCity.innerHTML = data.city;
     currentCity.innerHTML = `${data.city}, ${data.country}`;
 });
+
+async function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ua&units=metric&APPID=${tokenForWeather}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+getWeather("Minsk").then((data) => {
+    let latitudeConverter = coordinatesConverter(data.city.coord.lat);
+    let longitudeConverter = coordinatesConverter(data.city.coord.lon);
+    latitude.innerHTML = `Latitude: ${latitudeConverter}`;
+    longitude.innerHTML = `Longitude: ${longitudeConverter}`;
+});
+
+function coordinatesConverter(coordinate){
+    let absolute = Math.abs(coordinate);
+    let degrees = Math.floor(absolute);
+    let minutesNotTruncated = (absolute - degrees) * 60;
+    let minutes = Math.floor(minutesNotTruncated);
+    return `${degrees}°${minutes}'`;
+}
 
 function getDate() {
     let fullDate = new Date();
